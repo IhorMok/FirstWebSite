d8a7a16c965736ec3b012ec8a9a46d75ff1c361b
Ihor Mokriyenko <igor4ik08@gmail.com>
2020-03-01 22:28:46 +0200

    Second commit

--- myblog/views.py
+++ myblog/views.py
@@ -25,7 +25,17 @@ def index(request):
 
 def show(request, pk=1):
     post = Articles.objects.get(id=pk)
-    return render(request, "blog/post.html", {'post': post})
+    comments = Comment.objects.all()
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comm = form.save(commit=False)
+            comm.post = post
+            comm.save()
+    else:
+        form = CommentForm()
+    return render(request, "blog/post.html", {'post': post, 'form': form, 'comments': comments})
+
 
 def download(request):
     return render(request, 'blog/download.html')
