5516167c8149b7d560a27c1533dca396db63575a
Ihor Mokriyenko <igor4ik08@gmail.com>
2020-03-29 22:28:52 +0300

    March

--- firstwebsite/settings.py
+++ firstwebsite/settings.py
@@ -78,11 +78,12 @@ WSGI_APPLICATION = 'firstwebsite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': '/path/to/my.cnf',
+        }
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
